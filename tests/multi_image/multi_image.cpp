45104ca079f0305fd45ba51c08c66c049ff93719
Bibhas B <bibhas2@gmail.com>
2026-03-12 12:16:58 -0400

    Test for rendering multiple images in the same window

--- tests/multi_image/multi_image.cpp
+++ tests/multi_image/multi_image.cpp
@@ -1,7 +1,7 @@
-//An application to load and render SVG images.
-
+//An application that tests rendering multiple SVG images in the same window 
+// using the same device context.
 #include "framework.h"
-#include "svg_viewer.h"
+#include "multi_image.h"
 #include "../../../mgui/include/mgui.h"
 #include "../../svglib.h"
 
@@ -14,76 +14,62 @@
 #pragma comment(lib, "gdi32.lib")
 #pragma comment(lib, "comdlg32.lib")
 
-void check_throw(HRESULT hr) {
-    if (!SUCCEEDED(hr)) {
-        throw hr;
-    }
-}
-
 class MainWindow : public CFrame {
     SVGDevice device;
-    SVGImage image;
-    float scale = 1.0;
+    SVGImage images[4];
+	const wchar_t* image_files[4] = {L"peacock.svg", L"butterfly.svg", L"bird.svg", L"man.svg"};
+    float scales[4] = {0.25f, 0.15f, 0.5f, 0.5f };
 public:
-    
-    void create() {
-        CFrame::create(L"SVG Viewer", 800, 600, IDC_WINPAGES);
 
-		device.init(getWindow());
+    void create() {
+        CFrame::create(L"SVG Viewer", 800, 600, IDC_MULTIIMAGE);
+
+        device.init(getWindow());
+
+        for (int i = 0; i < 4; i++) {
+            if (!SVG::load(image_files[i], device, images[i])) {
+                errorBox((std::wstring(L"Failed to open or parse the SVG file: ") + image_files[i]).c_str());
+            }
+		}
     }
     void onClose() override {
         CWindow::stop();
-	}
+    }
     void onCommand(int id, int type, CWindow* source) override {
-        if (id == ID_FILE_OPEN) {
-            std::wstring filename;
-
-            if (!openFileName(L"Open SVG File",
-                { { L"SVG Files", L"*.svg" }, { L"All Files", L"*.*" } },
-                filename)) {
-                return;
-			}
-
-			if (SVG::load(filename.c_str(), device, image)) {
-				device.redraw();
-            }
-            else {
-				errorBox(L"Failed to open or parse the SVG file.");
-            }
-        }
-        else if (id == IDM_EXIT) {
+        if (id == IDM_EXIT) {
             onClose();
-        } else if (id == ID_VIEW_ZOOMIN) {
-            scale *= 1.25f;
-            device.redraw();
         }
-        else if (id == ID_VIEW_ZOOMOUT) {
-            scale /= 1.25f;
-            device.redraw();
-        } else if (id == ID_VIEW_ACTUALSIZE) {
-            scale = 1.0f;
-            device.redraw();
+    }
+
+    void renderImages() {
+        float x = 20.0f;
+
+		SVG::clear(device);
+
+        for (int i = 0; i < 4; i++) {
+            SVG::render(device, images[i], x, 20, scales[i]);
+
+            x += 200.0f;
         }
-	}
+    }
 
     bool handleEvent(UINT message, WPARAM wParam, LPARAM lParam) {
+
         switch (message) {
         case WM_PAINT:
             PAINTSTRUCT ps;
-
             //We must call BeginPaint and EndPaint to validate the
             //invalidated region, or else we will get continuous
             //WM_PAINT messages.
             BeginPaint(m_wnd, &ps);
-            SVG::clear(device);
-            SVG::render(device, image, 20, 20, scale);
+            renderImages();
             EndPaint(m_wnd, &ps);
             break;
         case WM_SIZE:
             device.resize();
             break;
         case WM_ERASEBKGND:
-			//Handle background erase to avoid flickering 
+            //Handle background erase to avoid flickering 
             //during resizing and move
             break;
         default:
@@ -95,11 +81,11 @@ public:
 };
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPWSTR    lpCmdLine,
-    _In_ int       nCmdShow)
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
 {
-    CWindow::init(hInstance, IDC_WINPAGES);
+    CWindow::init(hInstance, IDC_MULTIIMAGE);
 
     HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
 
